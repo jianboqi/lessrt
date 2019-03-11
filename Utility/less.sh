d722fe19dce3bfefeb425a583de79033dfe44256
Jianbo QI <jianboqi@126.com>
2019-03-11 15:36:19 +0800

    add linux build

---
+++ Utility/less.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+chmod +x ./bin/jre/bin/java
+chmod +x ./bin/python/python
+chmod +x ./bin/python/python3.5
+chmod +x ./bin/python/bin/python
+chmod +x ./bin/python/bin/python3.5
+./bin/jre/bin/java -jar LessGUI.jar
--- cpp/lesscpp/build-linux.py
+++ cpp/lesscpp/build-linux.py
@@ -4,9 +4,13 @@ if os.path.exists('config.py'):
 shutil.copy('config-linux.py','config.py')
 os.system("scons -j6")
 
+# build cpp and copy the file to python folder
 print "copying files..."
 from distutils.dir_util import copy_tree
 if not os.path.exists('../../python/lesspy/bin/rt/lessrt'):
 	os.makedirs('../../python/lesspy/bin/rt/lessrt')
 copy_tree("dist-linux","../../python/lesspy/bin/rt/lessrt")
+
+# copy libs of linux to the folder lessrt
+copy_tree("../../Utility/libs_linux/ubuntu16_04","../../python/lesspy/bin/rt/lessrt")
 print "finished."
