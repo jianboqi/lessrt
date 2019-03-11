156e5f37f53221b4c862b6279d06c632fc4bd374
Jianbo QI <jianboqi@126.com>
2019-03-11 16:12:11 +0800

    Fix Linux build bugs

---
+++ Utility/env_linux.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt install scons mercurial libpng12-dev libjpeg-dev libilmbase-dev libxerces-c-dev libboost-all-dev libopenexr-dev libglewmx-dev libxxf86vm-dev libpcrecpp0 libeigen3-dev libfftw3-dev
--- Utility/less.sh
+++ Utility/less.sh
@@ -4,4 +4,7 @@ chmod +x ./bin/python/python
 chmod +x ./bin/python/python3.5
 chmod +x ./bin/python/bin/python
 chmod +x ./bin/python/bin/python3.5
+chmod +x ./bin/scripts/Lesspy/bin/rt/lessrt/lessrt
+chmod +x ./bin/scripts/Lesspy/bin/rt/lessrt/lesssrv
+chmod +x ./bin/scripts/Lesspy/bin/rt/lessrt/mtsimport
 ./bin/jre/bin/java -jar LessGUI.jar
